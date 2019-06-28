a42890ff8007edf3dc18f86aeacdf4f1bc8959cc
anthonyrosasrodriguez <marco.rosas.20.2015@gmail.com>
2019-06-28 12:22:46 -0400

    Initial commit

---
+++ tethysapp/anthony_all_about_me/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class AnthonyAllAboutMe(TethysAppBase):
+    """
+    Tethys app class for Anthony All About Me.
+    """
+
+    name = 'Anthony All About Me'
+    index = 'anthony_all_about_me:home'
+    icon = 'anthony_all_about_me/images/icon.gif'
+    package = 'anthony_all_about_me'
+    root_url = 'anthony-all-about-me'
+    color = '#2980b9'
+    description = 'Place a brief description of your app here.'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='anthony-all-about-me',
+                controller='anthony_all_about_me.controllers.home'
+            ),
+        )
+
+        return url_maps
